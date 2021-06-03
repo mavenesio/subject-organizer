ededb222a68456fec448f417067e5ce7491a206a
mavenesio <mafiorito@fi.uba.ar>
2021-06-03 12:02:29 -0300

    adjust folders

--- src/App.js
+++ src/App.js
@@ -1,54 +1,8 @@
 import React, { useState } from 'react';
-import NetworkGraph from './components/NetworkGraph';
-import { getProgress, getAverage } from './components/utils';
-import FileUpload from './components/FileUpload';
+import NetworkGraph from './components/network-graph';
+import { getProgress, getAverage } from './components/network-graph/utils';
+import FileUpload from './components/file-upload';
 import './App.css';
-
-
-const subjs = [
-  { grade: 5, code: 'CBC',  correlatives: []},
-  { grade: 7, code: '61.03',correlatives: ['CBC']},
-  { grade: 6, code: '61.08',correlatives: ['CBC']},
-  { grade: 8, code: '75.40',correlatives: ['CBC']},
-  { grade: 0, code: '61.07',correlatives: ['CBC']},
-  { grade: 4, code: '75.03',correlatives: ['75.40']},
-  { grade: 5, code: '75.41',correlatives: ['75.40']},
-  { grade: 4, code: '61.09',correlatives: ['61.03','61.08']},
-  { grade: 4, code: '71.12',correlatives: ['CBC']},
-  { grade: 8, code: '75.06',correlatives: ['75.03','75.41']},
-  { grade: 8, code: '75.07',correlatives: ['75.41']},
-  { grade: 0, code: '71.13',correlatives: ['71.12']},
-  { grade: 8, code: '75.09',correlatives: ['75.06','75.07']},
-  { grade: 8, code: '75.42',correlatives: ['75.03','75.41']},
-  { grade: 0, code: '71.14',correlatives: ['61.03','61.07','61.08']},
-  { grade: 6, code: '75.10',correlatives: ['75.09']},
-  { grade: 5, code: '75.15',correlatives: ['75.09']},
-  { grade: 0, code: '71.15',correlatives: ['61.09','71.14']},
-  { grade: 0, code: '71.16',correlatives: ['71.12','71.14']},
-  { grade: 6, code: '75.17',correlatives: ['75.10']},
-  { grade: 9, code: '75.18',correlatives: ['71.13','71.16','75.17']}];
-const aa =[
-  { grade: 5, code: 'CBC',  correlatives: []},
-  { grade: 7, code: '61.03',correlatives: ['CBC']},
-  { grade: 6, code: '61.08',correlatives: ['CBC']},
-  { grade: 8, code: '75.40',correlatives: ['CBC']},
-  { grade: 0, code: '61.07',correlatives: ['CBC']},
-  { grade: 4, code: '75.03',correlatives: ['75.40']},
-  { grade: 5, code: '75.41',correlatives: ['75.40']},
-  { grade: 4, code: '61.09',correlatives: ['61.03','61.08']},
-  { grade: 4, code: '71.12',correlatives: ['CBC']},
-  { grade: 8, code: '75.06',correlatives: ['75.03','75.41']},
-  { grade: 8, code: '75.07',correlatives: ['75.41']},
-  { grade: 0, code: '71.13',correlatives: ['71.12']},
-  { grade: 8, code: '75.09',correlatives: ['75.06','75.07']},
-    { grade: 8, code: '75.42',correlatives: ['75.03','75.41']},
-  { grade: 0, code: '71.14',correlatives: ['61.03','61.07','61.08']},
-  { grade: 6, code: '75.10',correlatives: ['75.09']},
-  { grade: 5, code: '75.15',correlatives: ['75.09']},
-  { grade: 0, code: '71.15',correlatives: ['61.09','71.14']},
-  { grade: 0, code: '71.16',correlatives: ['71.12','71.14']},
-  { grade: 6, code: '75.17',correlatives: ['75.10']},
-  { grade: 9, code: '75.18',correlatives: ['71.13','71.16','75.17']}]
 
 const App = () => {
   const [subjects, setSubjects] = useState([]);
