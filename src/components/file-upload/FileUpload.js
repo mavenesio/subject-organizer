f98170422e46316635097ae33bba72290fcebaf7
mavenesio <mafiorito@fi.uba.ar>
2021-06-06 15:23:54 -0300

    add some validations

--- src/components/file-upload/FileUpload.js
+++ src/components/file-upload/FileUpload.js
@@ -8,7 +8,8 @@ import 'react-toastify/dist/ReactToastify.css';
 const checkCorrelatives = (subjects) => {
   const codes = subjects.map(subject => subject.code);
   subjects.forEach( (subject, index) => {
-    if (subject.correlatives.length > 0 && !subject.correlatives.every(correlative => codes.includes(correlative))){
+    if ((subject.correlatives.length > 0) && 
+        (!subject.correlatives.every(correlative => codes.includes(correlative)))){
       throw new Error(
         `Some of theses correlatives doesn't exist \"${subject.correlatives.join(' - ')}\". They are in line number ${index}`
       );
@@ -18,6 +19,7 @@ const checkCorrelatives = (subjects) => {
 
 const FileUpload = ({setSubjects}) => {
   const [parsedSubject, setParsedSubject] = useState();
+  const [status, setStatus] = useState('NOT_OVER');
 
   useEffect(() => {
     if(parsedSubject){
@@ -32,36 +34,59 @@ const FileUpload = ({setSubjects}) => {
   }, [parsedSubject]);
 
   const loadFile = (file) => {
-    try {
       var reader = new FileReader();
-      reader.onload = function(e) {
-        var contents = e.target.result;
-        const subjects = [];
-        contents.split("\n").forEach( (line, index) => {
-          const parsedLine = line.split(",");
-          subjects.push({
-            code: parsedLine[0].trim(),
-            grade: parseInt(parsedLine[1]),
-            credits: parseInt(parsedLine[2]),
-            name: parsedLine[3],
-            correlatives: parsedLine[4] ? parsedLine[4].split("-").map(x => x.trim()) : [],
-          })
-        })
-        setParsedSubject(subjects)
-      };
+      reader.onload = parseFile;
       reader.readAsText(file);
-    } catch (e){
-      console.log('aaaa', e);
+  }
+
+  const onOpenFileUploader = (e) => {
+    const fileSelector = document.createElement('input');
+    fileSelector.setAttribute('type', 'file');
+    fileSelector.addEventListener('change', (event) => {
+      const fileList = event.target.files;
+      loadFile(fileList[0])
+      console.log(fileList);
+    });
+    e.preventDefault();
+    fileSelector.click();
+  }
+
+  const parseFile = (e) => {
+    try {
+      var contents = e.target.result;
+      const subjects = [];
+      contents.split("\n").forEach( (line, index) => {
+        const parsedLine = line.split(",");
+        if(parsedLine.length !== 4 && parsedLine.length !== 5) {
+          throw new Error(
+            `Line number ${index + 1} has some inconsistency`
+          );
+        }
+        subjects.push({
+          code: parsedLine[0].trim(),
+          grade: parseInt(parsedLine[1]),
+          credits: parseInt(parsedLine[2]),
+          name: parsedLine[3],
+          correlatives: parsedLine[4] ? parsedLine[4].split("-").map(x => x.trim()) : [],
+        })
+      })
+      setParsedSubject(subjects);
+    } catch (err){
+      toast.error(err.message);
     }
   }
+
 
   return (
     <div className="container">
       <FileDrop
-         className="fileDrop"
-        onDrop={(files, event) => loadFile(files[0])}
+        className={(status === "NOT_OVER") ? "file-drop" : "file-drop__over"}
+        onDrop={(files) => loadFile(files[0])}
+        onDragOver={() => setStatus('DRAG_OVER')}
+        onDragLeave={() => setStatus('NOT_OVER')}
+        onTargetClick={onOpenFileUploader}
       >
-        Drop file here
+        ¡ Drop file here or click me !
       </FileDrop>
       <ToastContainer
         position="top-right"
