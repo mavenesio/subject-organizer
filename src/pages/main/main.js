628aa17862d552a59cdb2ad5a083d8adc86a4a4b
mavenesio <mafiorito@fi.uba.ar>
2021-06-09 15:11:43 -0300

    add empty side bar

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,28 +1,44 @@
 
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import NetworkGraph from './components/network-graph';
 import FileUpload from './components/file-upload';
 import Header from './components/header/header';
 import SubjectsContext from '../../context/subjects/subjectsContext';
+import { scaleRotate as Menu} from 'react-burger-menu'
 import './styles.css'
 
 const Main = () => {
   const { hasSubjects } = useContext(SubjectsContext);
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className="main-container">
-      <Header />
-      <div className="body-container">
-        <div className="card">
-          {
-            hasSubjects ? ( 
-              <NetworkGraph/>
-            ) : (
-              <FileUpload/>
-            )
-          }
+    <div
+      className="main-container"
+      id="outer-container"
+    >
+      <Menu
+        pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }
+        onStateChange={ setMenuOpen }
+        pushRotate
+      >
+        <div>HOLA</div>
+      </Menu>
+        <Header />
+        <div
+          className="body-container"
+          id="page-wrap"
+        >
+          <div className="card">
+            {
+              hasSubjects ? ( 
+                <NetworkGraph/>
+              ) : (
+                <FileUpload/>
+              )
+            }
+          </div>
         </div>
       </div>
-    </div>
   )
 }
   
